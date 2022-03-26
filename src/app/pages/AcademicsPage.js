8706411f1a723119361abef44bdf1724c352c5fc
huilin <huilin.zou@cix.csi.cuny.edu>
2022-03-25 23:21:31 -0400

    FEAT-flowchart btn

--- src/app/pages/AcademicsPage.js
+++ src/app/pages/AcademicsPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import SecYearTipBtn from '../components/SecYearTipBtn'
+import Flowchartbtn from '../components/Flowchartbtn'
 
 const AcademicsPage = () => {
   return (
     <div>
       <SecYearTipBtn/>
+      <Flowchartbtn/>
     </div>
 
 
