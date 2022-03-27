ce3d7977ba4d94181f1edd17346393fc7b433481
AndreaHabib <andrea.atef@yahoo.com>
2022-03-27 17:46:02 -0400

    new feature

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<App />}></Route>
+          <Route path="/" element={<App />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
