f15a5a605cbe6b7c64cc5f44dfb423678798c89f
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-04-15 19:34:10 -0400

    FEAT: Academic Page Card-Buttons with descriptions for further editing, a reusable component to add description, title, and other values for easier re-use. Might re-factor some code to make card portion a component, but for now this makes sense to me.

--- src/app/components/ButtonBasic.js
+++ src/app/components/ButtonBasic.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import PropTypes from "prop-types";
-import { Button} from '@mui/material';
-import { Typography } from '@mui/material';
+import { Button, Typography, Card, CardContent, CardActions} from '@mui/material';
 
-function ButtonBasic({title, description, size, color}) {
+function ButtonBasic({title, description, size, color, bgColor}) {
     return (
         <>
-            <Button variant='contained' size= {size} color = {color}>
-
-                <Typography variant='h3'> {title} </Typography>
+            <Card sx={{minWidth: 300}} style={{backgroundColor: bgColor, color: ""}}>
+                <CardContent>
+                    <Typography variant='h3'> {title} </Typography>
+                    <span>
                 <Typography variant = 'subtitle1'> {description} </Typography>
-
+                </span>
+                </CardContent>
+                <CardActions style = {{display: "flex", justifyContent: "center"}}>
+            <Button variant='contained' size= {size} color = {color}>
+                    <h5> Go to the section </h5>
             </Button>
+                </CardActions>
+            </Card>
         </>
     );
 }
 
-/*
+/* DEV @ ADAM 4.15.2022
 *  Title - name of the button
 *  color - primary, secondary, warning, success, etc.
+*  bg-color - any basic color in css (red, pink, blue etc.)
 *  description: description of the button
-*  size: small, lm
+*  size: small, medium, large
 * */
 
 ButtonBasic.propTypes = {
     title: PropTypes.string,
     color: PropTypes.string,
+    bgColor: PropTypes.string,
     size: PropTypes.string,
     description: PropTypes.string
 }
@@ -34,6 +42,7 @@ ButtonBasic.defaultProps = {
     title: "Test Button",
     color: "primary",
     size: "medium",
+    bgColor: "white",
     description: "I am a default test button prop"
 }
 
--- src/app/pages/AcademicsPage.js
+++ src/app/pages/AcademicsPage.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import {Grid} from "@mui/material";
 import ButtonBasic from "../components/ButtonBasic";
+
 import '../styles/AcademicPage.css';
 
 const AcademicsPage = () => {
   return (
     <div>
-        <ButtonBasic title="Adam" description="Test" size="large" color="primary"/>
+        <Grid container={true} spacing={3} mt={3} mb={3} direction="row" justifyContent="space-evenly" alignItems="end" >
+            <Grid item xs={3}>
+            <ButtonBasic title="Academics" description="Ea repellendus natus sed dolorem quam sed quia recusandae ut nemo voluptatem et be" size="large" color="primary"/>
+            </Grid>
+
+            <Grid item xs={3}>
+                <ButtonBasic title="Pathway" description="Ea repellendus natus sed dolorem quam sed quia recusandae ut nemo voluptatem et be" size="large" color="secondary"/>
+            </Grid>
+
+            <Grid item xs={3}>
+                <ButtonBasic title="Professional" description="Ea repellendus natus sed dolorem quam sed quia recusandae ut nemo voluptatem et be" size="large" color="success"/>
+            </Grid>
+        </Grid>
     </div>
 
   )
