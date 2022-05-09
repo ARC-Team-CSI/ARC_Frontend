52f8247e5b503871468804d0736d7c6a6f528e67
bigbl <adamkostandy@gmail.com>
2022-05-09 08:01:12 -0400

    CHANGE: Changed colors to Huilin's Document.

--- src/theme.js
+++ src/theme.js
@@ -3,10 +3,10 @@ import {createTheme} from "@mui/material";
 const theme = createTheme( {
     palette: {
         primary: {
-            main: "#85BBF3",
+            main: "#7FC0E6",
         },
         secondary: {
-            main: "#C4C4C4",
+            main: "#0072B1",
         },
         success: {
             main: "#49C1A4"
@@ -16,7 +16,7 @@ const theme = createTheme( {
             contrastText: "#fff"
         },
         info: {
-            main: "#1268D2",
+            main: "#C4C4C4",
             light: "#7FC0E6",
             dark: "#0072B1",
             contrastText: "#fff"
