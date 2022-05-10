fb647842e6cdaa9f4ae7be7a92151590727ec999
Victoria Jaczynski <victoria.jacz.14@gmail.com>
2022-05-09 22:16:48 -0400

    added header and new page

--- src/app/pages/PathwayChecklistPage.js
+++ src/app/pages/PathwayChecklistPage.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ResponsiveAppBar from "../components/ResponsiveAppBar";
 
 const PathwayChecklistPage = () => {
 
     return (
       <div>
+          <ResponsiveAppBar/>
         <p>This is the pathway checklist page. Next is to add the sidebar.</p>
       </div>
     );
