576f29ad81d708b690d2c90e7690f5a17c20aa7c
Unaiza898 <unaiza.nizami@cix.csi.cuny.edu>
2022-05-12 00:14:28 -0400

    drop down menu

--- src/app/components/ResponsiveAppBar.js
+++ src/app/components/ResponsiveAppBar.js
@@ -7,20 +7,38 @@ import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import { purple } from "@mui/material/colors";
 import { Container } from "react-bootstrap";
+
+
+
+// import { View, Image, StyleSheet } from 'react-native';
+
 import logo from "../assests/logo.png";
 import ARC from "../assests/ARC.png";
 import csilogo from "../assests/csilogo.png";
 import tips from "../assests/lightbulb.png";
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import Menu_icon from '../assests/menu_icon.png'
+import Avatar from '@mui/material/Avatar';
+import '../pages/Home';
+import { Link } from 'react-router-dom';
+// import PopupState from '@mui/material/Popover'
 
 function ResponsiveAppBar() {
-
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   return (
     <Box sx={{ flexGrow: 1 }} className="AppBar">
       <AppBar position="static" color="primary">
         <Toolbar>
-
-
+      
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             {/* <Image
         style={styles.tinyLogo}
@@ -50,7 +68,33 @@ function ResponsiveAppBar() {
               width="250px"
             />
           </Typography>
-         
+          <Button
+        id="basic-button"
+        aria-controls={open ? 'basic-menu' : undefined}
+        aria-haspopup="true"
+        aria-expanded={open ? 'true' : undefined}
+        ml={30}
+        onClick={handleClick}
+        startIcon={<Avatar src={Menu_icon} />}
+
+      >
+  
+      </Button>
+     
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          'aria-labelledby': 'basic-button',
+        }}
+      >
+        <MenuItem component ={Link} to= "/">Home</MenuItem>
+        <MenuItem onClick={handleClose}>Career</MenuItem>
+        
+      </Menu>
+    
         </Toolbar>
       </AppBar>
     </Box>
