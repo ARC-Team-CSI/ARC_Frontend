320230e69251941dde95188c30bb06f8df85f1dc
bigbl <adamkostandy@gmail.com>
2022-06-08 12:44:21 -0400

    CHANGE: Edited CSS of Home page and edited the JS document to contain a center box with LOGO

--- src/app/pages/Home.js
+++ src/app/pages/Home.js
@@ -3,13 +3,45 @@ import AcademicsBtn from '../components/AcademicsBtn'
 import ProfessionalBtn from '../components/ProfessionalBtn'
 import './Home.css'
 import ResponsiveAppBar from "../components/ResponsiveAppBar";
+import {Box} from "@material-ui/core";
+import {Grid} from "@mui/material";
+import logo from "../assests/logo.png"
 
 function Home() {
   return (
     <>
         <ResponsiveAppBar/>
-      <div><AcademicsBtn /></div>
-    <div><ProfessionalBtn /></div>
+
+        <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            className="grid"
+        >
+            <Grid item className="bgImage">
+
+                <Box sx={{
+                    width: "40rem",
+                    height: "40vh",
+                    backgroundColor: '#7FC0E6',
+                    paddingBottom: 10,
+                    flexDirection: "row",
+                    borderRadius: "30px",
+                    backgroundImage: `url(${logo})`,
+                    backgroundSize: "contain",
+                    backgroundRepeat: "no-repeat"
+                }}
+                >
+                </Box>
+
+            </Grid>
+
+        </Grid>
+
+        <div><AcademicsBtn /></div>
+        <div><ProfessionalBtn /></div>
+
     </>
   )
 }
