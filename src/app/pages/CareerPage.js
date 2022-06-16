57c68edf93c728aa59ec00bc0a8b6f34f6868d24
diabeatz96 <75499553+diabeatz96@users.noreply.github.com>
2022-06-16 12:31:16 -0400

    CHANGES: FIXED BUTTON LOCATION AND CREATED SEP CAREER PAGE.JS

--- src/app/pages/CareerPage.js
+++ src/app/pages/CareerPage.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import {Box, Typography} from "@mui/material";
+import {Box, Button, Grid, Typography} from "@mui/material";
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
+import '../styles/CareerPage.css';
+
 
 const CareerPage = () => {
   return (
 
-    <div className= "containerSplit">
+    <Grid container className = "containerSplit" flexDirection="column">
     <ResponsiveAppBar/>
         <div className = "firstHalf">
             <Typography variant="h2" component="h1" pt={10} px={10} >
@@ -16,8 +18,19 @@ const CareerPage = () => {
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, atque deserunt, dolore impedit ipsum molestiae officia perspiciatis quasi quisquam rerum totam voluptatem! Aperiam deleniti labore magnam, maiores minus odio placeat.
             </Typography>
             </Box>
+
+            <Grid container flexDirection="row" justifyContent="flex-end" marginY={25} justifySelf="self-end">
+                <Grid item xs={2}>
+                    <Button variant="contained" style={{maxWidth: '130px', maxHeight: '80px', minWidth: '130px', minHeight: '80px'}}> Resume </Button>
+                </Grid>
+                <Grid item xs={4}>
+                    <Button variant="contained"  style={{maxWidth: '130px', maxHeight: '80px', minWidth: '130px', minHeight: '80px'}}> Career/Degree </Button>
+                </Grid>
+            </Grid>
+
+
         </div>
-  </div>
+    </Grid>
 
   )
 }
