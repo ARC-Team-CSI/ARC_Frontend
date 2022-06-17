0642b4a48086d235400165391830f44f0eeae5e6
Amena Foshanji <amena.foshanji@gmail.com>
2022-06-17 11:49:22 -0400

    Images for career page buttons

--- src/app/pages/CareerPage.js
+++ src/app/pages/CareerPage.js
@@ -1,6 +1,8 @@
 import React from 'react'
-import {Box, Typography} from "@mui/material";
+import {Box, Grid, Typography} from "@mui/material";
+//import {Box, Typography} from "@mui/material";
 import ResponsiveAppBar from '../components/ResponsiveAppBar';
+import ButtonRouter from "../components/ButtonRouter"; 
 
 const CareerPage = () => {
   return (
@@ -16,6 +18,30 @@ const CareerPage = () => {
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, atque deserunt, dolore impedit ipsum molestiae officia perspiciatis quasi quisquam rerum totam voluptatem! Aperiam deleniti labore magnam, maiores minus odio placeat.
             </Typography>
             </Box>
+        </div>
+        <div className="secondHalf">
+            <Grid container sx={{flexDirection: {xs: "column", md: "row"}, justifyContent: {xs: "center", md: "space-around"}, alignContent: {xs: "center", md: "space-around"}}}  >
+                <Grid item xs={3} mt={5}>
+                    <ButtonRouter title="Career/Degree" size="small" color="primary" link="http://flowcharts-csi.vercel.app/" image = "/images/FlowChart.png"/>
+                </Grid>
+
+                <Grid item xs={3} mt={5}>
+
+                    <ButtonRouter title="Resume" size="small" color="secondary" link="/PathwayChecklistPage" image = "/images/PCheckList.png"/>
+                </Grid>
+
+                <Grid item xs={3} mt={5}>
+                    <ButtonRouter title="Internship/Jobs" size="small" color="success" image = "/images/StdTip.jpg" link= "/StudentTips"/>
+                </Grid>
+            </Grid>
+            <Grid item xs={4} mt={5}>
+                    <ButtonRouter title="Gitbook" size="small" color="success" image = "/images/StdTip.jpg" link= "/StudentTips"/>
+                </Grid>
+                <Grid item xs={3} mt={5}>
+                    <ButtonRouter title="CS Event" size="small" color="success" image = "/images\CSevent.jpg" link= "/StudentTips"/>
+                </Grid>
+            
+
         </div>
   </div>
 
